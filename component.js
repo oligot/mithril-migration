3f6847264e03bd810957e834e8dbc36912855c2f
Olivier Ligot <oligot@gmail.com>
2017-03-12 15:53:19 +0100

    Initial commit

---
+++ component.js
@@ -0,0 +1,27 @@
+var Component = (function() {
+  function controller() {
+    var vnode = {
+      state: {}
+    }
+    oninit(vnode);
+    return {state: vnode.state};
+  }
+
+  function oninit(vnode) {
+    vnode.state.name = stream('');
+  }
+
+  function view(vnode, attrs) {
+    vnode.attrs = vnode.attrs /* Mithril 1.x */ || attrs; /* Mithril 0.2 */
+    return m('main', [
+      m('h1', 'Hello ' + vnode.state.name()),
+      m('input', {oninput: m.withAttr('value', vnode.state.name)})
+    ]);
+  }
+
+  return {
+    controller,
+    oninit,
+    view
+  };
+})();
---
+++ readme.md
@@ -0,0 +1,6 @@
+A small project that shows how to write a shared Mithril component that can be used by Mithril 0.2 and 1.x applications
+
+Here is the project structure :
+* [component.js](component.js): the shared component
+* [0.2.html](0.2.html): a simple Mithril 0.2 application that uses the component
+* [1.x.html](1.x.html): a simple Mithril 1.x application that uses the component
